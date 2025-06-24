dcfce066137327895bd79548707270529bf69393
ComfyRandom <comfyrandom@gmail.com>
2025-06-24 18:59:36 +0500

    Удален лишний импорт

--- src/components/catalog/ProductCard.tsx
+++ src/components/catalog/ProductCard.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart, faInfoCircle, faMapMarkerAlt, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faShoppingCart, faInfoCircle, faUser } from "@fortawesome/free-solid-svg-icons";
 import { faBitcoin } from "@fortawesome/free-brands-svg-icons";
 import {Link} from "react-router-dom";
 
